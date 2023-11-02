270d741ccdc7293b244452e1d63f838ac09d8cff
Frost Ming <me@frostming.com>
2023-11-02 14:16:57 +0800

    fix: remove one challenge
    Signed-off-by: Frost Ming <me@frostming.com>

--- challenges/advanced-callable/question.py
+++ challenges/advanced-callable/question.py
@@ -1,48 +1,35 @@
 """
 TODO:
 
-Define a callable type that takes two arguments:
-
-1. *arbitrary param name*: a string
-2. `size`: an integer
-
-Returns: None
+Define a callable type that accepts a string parameter called `name` and returns None.
 
 HINT: Use Protocol
 """
 
 
-class MyCallable:
+class SingleStringInput:
     ...
 
 
 def should_pass():
-    def test_func(func: MyCallable) -> None:
+    def accept_single_string_input(func: SingleStringInput) -> None:
+        func(name="name")
+
+    def string_name(name: str) -> None:
         ...
 
-    def name_size(name: str, size: int) -> None:
-        ...
-
-    def type_size(type_: str, size: int) -> None:
-        ...
-
-    test_func(name_size)
-    test_func(type_size)
+    accept_single_string_input(string_name)
 
 
 def should_fail():
-    def test_func(func: MyCallable) -> None:
+    def accept_single_string_input(func: SingleStringInput) -> None:
+        func(name="name")
+
+    def string_value(value: str) -> None:
         ...
 
-    def age_size(age: int, size: int) -> None:
-        ...
-
-    def name_age(name: str, age: int) -> None:
-        ...
-
-    def return_string(name: str, size: int) -> str:
+    def return_string(name: str) -> str:
         return name
 
-    test_func(age_size)
-    test_func(name_age)
-    test_func(return_string)
+    accept_single_string_input(string_value)
+    accept_single_string_input(return_string)
