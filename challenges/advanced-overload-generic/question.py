6f23f6c7cd51f09cb2c48fdda1922350423ede82
RF-Tar-Railt <3165388245@qq.com>
2023-11-02 17:54:44 +0800

    new challenge

---
+++ challenges/advanced-overload-generic/question.py
@@ -0,0 +1,24 @@
+"""
+TODO:
+
+foo is a function that returns an interger when called specified Foo[int], returns a string when called specified Foo[str], otherwise it returns a Foo[T].
+"""
+from typing import TypeVar, Generic
+
+class Foo(Generic[T]): ...
+    a: T
+
+def foo(value: Foo):
+    ...
+
+
+def should_pass():
+    foo(Foo[int]()).bit_length()
+    foo(Foo[str]()).upper()
+    foo(Foo[list]()).a.append(1)
+
+
+def should_fail():
+    foo(Foo[int]()).upper()
+    foo(Foo[str]()).bit_length()
+    foo(Foo[list]()).bit_length()
---
+++ challenges/extreme-constructor-parameter/question.py
@@ -0,0 +1,31 @@
+"""
+TODO:
+
+Define a decorator `constructor_parameter` that input target class 
+and return a wrapper function with the same signature as the constructor of the target class, 
+and function which decorated by `constructor_parameter` can be called with the instance of the target class.
+"""
+
+class Foo:
+    def __init__(self, a: int, b: str) -> None:
+        ...
+
+def constructor_parameter():
+    ...
+
+
+def should_pass():
+    @constructor_parameter(Foo)
+    def func(foo: Foo) -> None:
+        ...
+
+    func(1, "2")
+
+
+def should_fail():
+    @constructor_parameter(Foo)
+    def func(foo: Foo) -> None:
+        ...
+
+    func("1", "2")
+    func([1, 2, 3])
