b644cf05157713d2a9bba7a3fef290770166b2bf
Frost Ming <me@frostming.com>
2023-11-04 21:22:24 +0800

    fix: update the code based on feedbacks
    Signed-off-by: Frost Ming <me@frostming.com>

--- views/utils.py
+++ views/utils.py
@@ -5,7 +5,7 @@ import re
 import tokenize
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import TypeAlias
+from typing import ClassVar, TypeAlias
 
 from mypy import api
 
@@ -14,16 +14,12 @@ MYPY_CONFIG = ROOT_DIR / "pyproject.toml"
 
 
 ChallengeName: TypeAlias = str
-CODE_SPLITTER = "\n## End of your code ##\n"
-EXPECT_ERROR_COMMENT = "expect-type-error"
-
-# Each mypy error should look like: `<filename>:<line_number>: <error|note>: <message>`
-# Here we only capture the error messages and line numbers
-MYPY_MESSAGE_REGEX = r"^(?:.+?):(\d+):(\s*error:.+)$"
 
 
 @dataclass
 class Challenge:
+    CODE_SPLITTER: ClassVar[str] = "\n## End of your code ##\n"
+
     name: ChallengeName
     difficulty: str
     code: str
@@ -34,7 +30,7 @@ class Challenge:
         self.parse_code()
 
     def parse_code(self):
-        self.user_code, _, self.test_code = self.code.partition(CODE_SPLITTER)
+        self.user_code, _, self.test_code = self.code.partition(self.CODE_SPLITTER)
 
 
 @dataclass(frozen=True, slots=True)
@@ -51,6 +47,12 @@ class TypeCheckResult:
 
 
 class ChallengeManager:
+    EXPECT_ERROR_COMMENT = "expect-type-error"
+
+    # Each mypy error should look like: `<filename>:<line_number>: <error|note>: <message>`
+    # Here we only capture the error messages and line numbers
+    MYPY_MESSAGE_REGEX = r"^(?:.+?):(\d+):(\s*error:.+)$"
+
     def __init__(self):
         self.challenges = self._load_challenges()
         self.challenge_names = [
@@ -85,36 +87,39 @@ class ChallengeManager:
 
         return challenges
 
-    @staticmethod
-    def _type_check_with_mypy(code: str) -> TypeCheckResult:
+    @classmethod
+    def _type_check_with_mypy(cls, code: str) -> TypeCheckResult:
         buffer = io.StringIO(code)
+        # This produces a stream of TokenInfos, example:
+        # TokenInfo(type=4 (NEWLINE), string='\n', start=(4, 3), end=(4, 4), line='"""\n'),
+        # TokenInfo(type=62 (NL), string='\n', start=(5, 0), end=(5, 1), line='\n')
+        # See https://docs.python.org/3/library/tokenize.html#tokenize.tokenize for more details
         tokens = list(tokenize.generate_tokens(buffer.readline))
         # Find all lines that are followed by a comment # expect-type-error
-        expect_error_lines = [
+        expect_error_line_numbers = [
             token.start[0]
             for token in tokens
             if token.type == tokenize.COMMENT
-            and token.string[1:].strip() == EXPECT_ERROR_COMMENT
+            and token.string[1:].strip() == cls.EXPECT_ERROR_COMMENT
         ]
         raw_result = api.run(["--config-file", str(MYPY_CONFIG), "-c", code])
         error_lines: list[str] = []
 
         for line in raw_result[0].splitlines():
-            m = re.match(MYPY_MESSAGE_REGEX, line)
+            m = re.match(cls.MYPY_MESSAGE_REGEX, line)
             if m is None:
                 continue
-            line_number = int(m.group(1))
-            message = m.group(2)
-            if line_number in expect_error_lines:
+            line_number, message = int(m.group(1)), m.group(2)
+            if line_number in expect_error_line_numbers:
                 # Each reported error should be attached to a specific line,
                 # If it is commented with # expect-type-error, let it pass.
-                expect_error_lines.remove(line_number)
+                expect_error_line_numbers.remove(line_number)
                 continue
             error_lines.append(f"{line_number}:{message}")
 
         # If there are any lines that are expected to fail but not reported by mypy,
         # they should be considered as errors.
-        for line_number in expect_error_lines:
+        for line_number in expect_error_line_numbers:
             error_lines.append(f"{line_number}: error: Expected type error")
 
         passed = len(error_lines) == 0
