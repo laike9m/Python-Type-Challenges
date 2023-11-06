43483a399bcbeb0b92656e1ee00d5d1ebd3a8944
laike9m <laike9m@gmail.com>
2023-11-05 22:29:11 -0800

    Fixed test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,14 +7,14 @@ a: int = 1
 b: str = "2"  # expect-type-error
 c: int = "3"  # expect-type-error
 d: str = 4
-e: list[str] = [
-    i  # expect-type-error
-    for i in range(10)
+e: list[str] = [  # expect-type-error
+    i for i in range(10)
 ]
 """
     result = ChallengeManager._type_check_with_pyright(code)
     assert result.passed is False
-    errors = [line for line in result.stdout.splitlines() if line]
-    assert len(errors) == 3
-    assert errors[0].startswith("4: error: Incompatible types in assignment")
-    assert errors[1].startswith("2: error: Expected type error")
+    assert result.stdout == (
+        '4:error: Expression of type "Literal[4]" cannot be assigned to declared type "str"\n'
+        "2: error: Expected type error\n\n"
+        "Found 2 errors"
+    )
