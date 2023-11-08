795aa6abcaa0cd7d39f6f79959cbe70ffe9176e0
laike9m <laike9m@gmail.com>
2023-11-07 20:29:37 -0800

    Add a test case for intermediate-generic

--- challenges/intermediate-generic/question.py
+++ challenges/intermediate-generic/question.py
@@ -15,3 +15,4 @@ from typing import List, assert_type
 assert_type(add(1, 2), int)
 assert_type(add("1", "2"), str)
 assert_type(add(["1"], ["2"]), List[str])
+assert_type(add(1, "2"), int)  # expect-type-error
