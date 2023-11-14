b8262da58e480c6a82cd513d56b822decb42c49e
laike9m <laike9m@users.noreply.github.com>
2023-11-13 22:29:42 -0800

    Update solution.py

--- challenges/advanced-recursive/solution.py
+++ challenges/advanced-recursive/solution.py
@@ -3,8 +3,10 @@ TODO:
 
 Define a `Tree` type. `Tree` is a dictionary, whose keys are string, values are also `Tree`.
 """
-from typing import Dict
+from typing import Dict, TypeAlias
 
+# For Python < 3.12
+# Tree : TypeAlias = Dict[str, 'Tree']
 
 type Tree = Dict[str, 'Tree']
 
