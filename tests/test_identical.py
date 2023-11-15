968bb4deb134885051da7e4a8592cdb88d6e170f
HUANG Jiaqi <wanderhuang@gmail.com>
2023-11-15 10:08:24 +0800

    reformat test_identical.py

--- tests/test_identical.py
+++ tests/test_identical.py
@@ -1,4 +1,3 @@
-
 """
 Make sure all test codes in questions and solutions are identical.
 """
@@ -20,15 +19,13 @@ def test_identical(solution_file: Path):
     with solution_file.open() as f:
         solution_code = f.read()
     solution_test = Challenge(
-        name=challenge_name + "solution",
-        level=Level(level),
-        code=solution_code).test_code
+        name=challenge_name, level=Level(level), code=solution_code
+    ).test_code
 
     question_file = solution_file.parent / QUESTION
     with question_file.open() as f:
         question_code = f.read()
     question_test = Challenge(
-        name=challenge_name + "question",
-        level=Level(level),
-        code=question_code).test_code
+        name=challenge_name, level=Level(level), code=question_code
+    ).test_code
     assert solution_test.strip() == question_test.strip()
