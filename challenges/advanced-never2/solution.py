cb6bae8027a3c8558193f7a15e5b00d8eb71312e
laike9m <laike9m@gmail.com>
2023-11-22 01:21:26 -0800

    Add challenge

--- challenges/advanced-never2/solution.py
+++ challenges/advanced-never2/solution.py
@@ -4,3 +4,9 @@ from typing import Never
 def stop() -> Never:
     """TODO: implement this function to make it type check"""
     raise RuntimeError("")
+
+
+## End of your code ##
+from typing import assert_never
+
+assert_never(stop())
