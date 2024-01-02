c6ca3a1be63ee4f72ff3e84cc906dcf082247de8
laike9m <laike9m@gmail.com>
2024-01-02 13:50:31 -0800

    Fix unexpected error in the literalstring challenge, fixes #81

--- challenges/intermediate-literalstring/question.py
+++ challenges/intermediate-literalstring/question.py
@@ -3,13 +3,15 @@ TODO:
 
 You're writing a web backend.
 Annotate a function `execute_query` which runs SQL, but also can prevent SQL injection attacks.
+
+NOTE: You don't need to implement `execute_query`
 """
 
 from typing import Iterable
 
 
 def execute_query(sql, parameters: Iterable[str] = ...):
-    """No need to implement it"""
+    ...
 
 
 ## End of your code ##
--- challenges/intermediate-literalstring/solution.py
+++ challenges/intermediate-literalstring/solution.py
@@ -3,6 +3,8 @@ TODO:
 
 You're writing a web backend.
 Annotate a function `execute_query` which runs SQL, but also can prevent SQL injection attacks.
+
+NOTE: You don't need to implement `execute_query`
 """
 
 from typing import LiteralString, Iterable
