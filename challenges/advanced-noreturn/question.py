0a440137898c61c3898cbaaab722127027304a26
Frost Ming <me@frostming.com>
2024-01-19 09:56:00 +0800

    new challenge: NoReturn(advanced)
    Signed-off-by: Frost Ming <me@frostming.com>

---
+++ challenges/advanced-noreturn/question.py
@@ -0,0 +1,21 @@
+"""
+TODO:
+
+Please add type hints to the function `abort` to make the test pass.
+"""
+
+
+def abort():
+    pass
+
+
+## End of your code ##
+from typing import assert_type
+
+
+def main(arg: int | None) -> None:
+    if arg is None:
+        abort()
+
+    print(arg + 42)
+    assert_type(arg, int)
---
+++ challenges/advanced-noreturn/solution.py
@@ -0,0 +1,22 @@
+"""
+TODO:
+
+Please add type hints to the function `abort` to make the test pass.
+"""
+from typing import NoReturn
+
+
+def abort() -> NoReturn:
+    pass
+
+
+## End of your code ##
+from typing import assert_type
+
+
+def main(arg: int | None) -> None:
+    if arg is None:
+        abort()
+
+    print(arg + 42)
+    assert_type(arg, int)
