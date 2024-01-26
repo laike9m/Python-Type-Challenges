fc86ddf292b09d1a77cee48fff606180ddbb2d07
F-park <52167622+F-park@users.noreply.github.com>
2024-01-26 11:26:41 +0800

    add comment

--- tests/test_hints.py
+++ tests/test_hints.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 
 def test_link(hint_file: Path):
+    # Verify if python docs url link to English documentation
     matches = re.findall(
         r"https://docs.python.org/[a-zA-Z]+-?[a-zA-Z]+",
         hint_file.read_text(encoding="utf-8"),
