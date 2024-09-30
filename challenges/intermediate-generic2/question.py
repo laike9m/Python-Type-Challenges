26f7222bfc3058f41eecf0d76930db9b3411423c
laike9m <laike9m@gmail.com>
2024-09-30 16:46:01 -0700

    Correct the description of generic2, fixed #124

--- challenges/intermediate-generic2/question.py
+++ challenges/intermediate-generic2/question.py
@@ -2,7 +2,7 @@
 TODO:
 
 The function `add` accepts two arguments and returns a value, they all have the same type.
-The type can only be str or int (or their subclasses).
+The type can only be str or int.
 """
 
 
--- challenges/intermediate-generic2/solution.py
+++ challenges/intermediate-generic2/solution.py
@@ -2,7 +2,7 @@
 TODO:
 
 The function `add` accepts two arguments and returns a value, they all have the same type.
-The type can only be str or int (or their subclasses).
+The type can only be str or int.
 """
 # For Python < 3.12
 # from typing import TypeVar
