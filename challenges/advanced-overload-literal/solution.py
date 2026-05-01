18ce0666068515f294f0cc3f1937c3b0ec80e3f7
LIghtJUNction <lightjunction.me@gmail.com>
2026-05-01 09:30:52 +0800

    Clean up imports in solution.py (#132)
    Removed unused TypeVar import for compatibility with Python 3.12.

--- challenges/advanced-overload-literal/solution.py
+++ challenges/advanced-overload-literal/solution.py
@@ -4,7 +4,7 @@ TODO:
 foo is a function that:
 returns an interger when second argument is 1, returns a string when second argument is 2, returns a list when second argument is 3, and otherwise it returns the same type as the first argument.
 """
-from typing import Any, Literal, overload, TypeVar
+from typing import Any, Literal, overload
 
 # Before 3.12 you have to write:
 # T = TypeVar('T')
