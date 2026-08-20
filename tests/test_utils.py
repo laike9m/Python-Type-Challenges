794ee35874aa45525823727c2d6bd5bd44d57bad
laike9m <laike9m@gmail.com>
2026-08-20 08:06:14 +0800

    Fix test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -15,7 +15,7 @@ e: list[str] = [  # expect-type-error
     result = ChallengeManager._type_check_with_pyright(user_code="", test_code=code)
     assert result.passed is False
     assert result.message == (
-        '4:error: Expression of type "Literal[4]" cannot be assigned to declared type "str"\n'
+        '4:error: Type "Literal[4]" is not assignable to declared type "str"\n'
         "2: error: Expected type error but instead passed\n\n"
         "Found 2 errors"
     )
